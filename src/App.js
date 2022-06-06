1ad819f770c68ac45a66770a26a92e7642a79b7f
kenichi <xianyigoben53@gmail.com>
2022-06-06 22:12:31 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { SignUp } from "./components/signup";
+import { Signin } from "./components/signin";
+import { Books } from "./components/books";
+import { Profile } from "./components/profile";
+import { Newbook } from "./components/newbook";
+import { Detail } from "./components/detail";
+import { Edit } from "./components/edit";
+import { Publicbooks } from "./components/publicbooks";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Books />}/>
+          <Route path="/detail/:id" element={<Detail />}/>
+          <Route path="/edit/:id" element={<Edit />} />
+          <Route path="/signup" element={<SignUp/>} />
+          <Route path="/signin" element={<Signin/>}/>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/new" element={<Newbook />}/>
+          <Route path="/sample" element={<Publicbooks/>}/>
+        </Routes>      
+      </BrowserRouter>
     </div>
   );
 }
